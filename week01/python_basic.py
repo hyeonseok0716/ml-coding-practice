f180e0e84e95fc576d6ede10d5f8eb8119505959
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-13 15:15:25 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -28,14 +28,14 @@ print(head + tail)
 print(head * 2)
 print("=" * 5)
 
-#문자열 인덱싱
+# 문자열 인덱싱
 a = "Now is better than never"
 print(a[0])
 print(a[4])
 print(a[-1])
 print(a[-2])
 
-#문자열 슬라이싱
+# 문자열 슬라이싱
 b = a[0] + a[1] + a[2]
 print(b)
 
