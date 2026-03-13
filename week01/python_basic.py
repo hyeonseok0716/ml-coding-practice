1fc3882009f53257b9533011875afc66a7b518c7
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-13 13:54:52 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -296,4 +296,34 @@ for score in [90, 25, 67, 45, 93]:
         print("%d번 학생은 합격입니다." % number)
     else:
         print("%d번 학생은 불합격입니다." % number)
-        
+
+# while 문
+i = 0
+while i < 5:
+    i += 1
+    print('*' * i)
+
+# 함수 정의
+def sum1(a, b):
+    x = a + b
+    return x
+
+def sum2(*args):
+    x = 0
+    for i in args:
+        x += i
+    return x
+
+# 함수 호출
+a = 5
+b = 3
+print(sum1(a, b))
+print(sum1(3, 5))
+print(sum2(1, 2, 3, 4, 5))
+print(sum2(2, 3.5, 10))
+
+"""## 내장 함수"""
+
+# 숫자 x의 절대값을 반환
+print(abs(-3.5))
+
