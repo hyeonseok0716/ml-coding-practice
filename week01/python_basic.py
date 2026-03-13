7708ee50c331d2835681096e192189ae74ffea5d
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-13 14:15:52 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -423,4 +423,9 @@ f.close()
 # 파일 쓰기
 f = open('example.txt', 'w')
 for i in range(1, 6):
-    data = ''
+    data = '%d번째 줄입니다. \n' % i
+    f.write(data)
+f.close()
+
+# 파일 모드 'r' - readline()
+f = open('example.txt', 'r')
