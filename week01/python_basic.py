7bcb6b3bc975403e24777953f6bcc2502704b01e
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-13 13:49:52 +0900

    Updated Changes

--- week01/python_basic.py
+++ week01/python_basic.py
@@ -271,4 +271,11 @@ print(1 in [1, 2, 3])
 print(x in [1, 2, 3])
 print(x not in [1, 2, 3])
 print('a' in ['a', 'b', 'c', 'd'])
-print(1 in [1, 2, 3])
+print('i' not in 'Python')
+
+# 아무 것도 하지 않게 설정
+if money >= 10:
+    pass
+else:
+    print('저금하자!')
+    
