e711c311e0506b066e3d047c6fa964b83cefb1f0
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 22:55:58 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -109,4 +109,11 @@ print(iris.drop('class', axis=1).agg(['sum','mean', 'std']))
 # print(iris.agg(['sum','mean', 'std']))    # class 컬럼으로 인해 에러 발생
 
 
-print(iris['class'].nuique())
+print(iris['class'].unique())
+print(iris['class'].nunique())
+print(iris['class'].value_counts())
+
+# 예제 데이터 생성
+data = {
+    'name': [ ]
+}
