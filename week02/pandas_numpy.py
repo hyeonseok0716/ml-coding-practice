4616354de7a0f199b4cb640a82f2e31aea374d26
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-20 00:08:02 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -46,7 +46,7 @@ print(sample_df.drop(['a','b','c'], axis=0))         # 행을 제거하려면 ax
 netflix = pd.read_csv('2.1.1.netflix.csv')
 print(netflix.head())
 
-print(netflix['releas_year'])
+print(netflix['release_year'])
 print(netflix['release_year'] > 2015)
 
 more2015 = netflix[netflix['release_year'] > 2015]
