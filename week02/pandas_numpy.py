181b8aedb7c1ac9040239c6f020c64ce9669810f
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 23:06:59 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -153,4 +153,11 @@ import numpy as np
 
 print(np.array([1,2,3]))
 
-print(np.array([]))
+print(np.array([[1,2,3],
+                [4,5,6],
+                [7,8,9]]))
+
+print(np.array([[[1,2,3],
+                 [4,5,6],
+                 [7,8,9]],
+                 [4,5,6]]))
