ecb1e65b014c4233c6740e27c632da13436f74d8
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 22:35:28 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -58,5 +58,5 @@ print(less2015.head())
 
 print((netflix['release_year'] > 2015) & (netflix['type'] == 'TV Show'))
 
-more2025_tv = netflix[(netflix['release_year' > 2015] & (netflix['type'] == 'TV show'))]
+more2015_tv = netflix[(netflix['release_year' > 2015] & (netflix['type'] == 'TV show'))]
 print(more2015_tv.head())
