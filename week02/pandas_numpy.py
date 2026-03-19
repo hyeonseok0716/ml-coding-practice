ff7e02b1c260126a0735debd6195358a9b2f4cf1
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 17:06:09 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -15,4 +15,7 @@ a = pd.DataFrame(sample_dic)
 a.info()
 
 pd.DataFrame([[1,2,],[3,4],[5,6],[7,8]])
-pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns = ['var_1',])
+pd.DataFrame([[1,2],[3,4],[5,6],[7,8]], columns = ['var_1','var_2'], index=['a','b','c','d'])
+
+import pandas as pd
+file_url = 'https://media.githubusercontent.com/media/musthave-ML10/data_source/main/sample_df.csv'
