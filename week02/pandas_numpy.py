8aacce6633fffff42f044debe688e5033f93cb48
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 22:27:58 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -39,4 +39,4 @@ print(sample_df.iloc[0:2])
 print(sample_df.iloc[0:3])
 print(sample_df.iloc[0:3, 2:4])        # 컬럼까지 동시에 인덱싱
 
-print(sample_df.drop(['ver_1,'ver_3']))
+print(sample_df.drop(['ver_1','ver_3'], axis=1))     #
