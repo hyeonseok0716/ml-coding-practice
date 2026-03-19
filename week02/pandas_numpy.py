58b98c177ae20f528fa03efbd2fc84722fcfe56c
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-19 23:30:29 +0900

    Updated Changes

--- week02/pandas_numpy.py
+++ week02/pandas_numpy.py
@@ -216,9 +216,9 @@ print(np.arange(8).reshape(2, 4) + 10)
 a = np.arange(8).reshape(2,4) ** 2
 print(a)
 
-print(a.sum())
-print(a.mean())
-print(a.mean(axis = 0))
+print(a.sum())     # 모든 요소의 합
+print(a.mean())    # 모든 요소의 평균
+print(a.mean(axis = 0))    # 열을 기준으로 연산
 
 print(a.min())
 print(a.max())
