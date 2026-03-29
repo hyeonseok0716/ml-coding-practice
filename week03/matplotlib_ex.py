953e29d3726c3c50651f2d4bd4cee72a7e0f1e92
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-29 18:15:11 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -311,3 +311,6 @@ plt.legend(loc='upper right')
 
 # 제목 설정 
 plt.suptitle('Survival Analysis by Number of Parents/Children (Parch) on the Titanic')
+plt.tight_layout()
+plt.savefig('Figure13.png')
+plt.close()
