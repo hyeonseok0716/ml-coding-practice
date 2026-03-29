cfb35a8d2cb8dc456088904064cdc60ef781eed9
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-29 17:44:41 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -187,4 +187,13 @@ plt.grid(True)
 plt.savefig('Figure08.png')
 plt.close()
 
-## 
+## **박스 플롯 : 승객 나이의 데이터 분포, 중앙값, 이상치 살펴보기**
+
+# 결측치 처리
+titanic = titanic.dropna(subset=['Age'])
+print(titanic.info())
+
+# 승객 등급에 따른 나이의 박스 플롯
+plt.boxplot([titanic[titanic['Pclass'] == 1]['Age'],
+             titanic[titanic['Pclass'] == 2]['Age'],
+             titanic[titanic['Pclass'] == 3]['Age'],])
