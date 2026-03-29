fe465a65c8891158e0495d4992f8c1dbfbc8c538
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-29 18:58:42 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -290,7 +290,7 @@ y2 = parch_counts[1].values # 생존자
 
 plt.figure(figsize=(10, 10))
 
-# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 하니씩 추가
+# plt.subplot() 함수를 사용하여 하나의 그림인 figure에 개별 서브플롯을 하나씩 추가
 # 첫 번째 서브플롯 설정(선 그래프)
 plt.subplot(2, 1, 1)
 # 선 그래프 그리기
@@ -298,7 +298,7 @@ plt.plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7,
 label='Not Survived')
 plt.xlabel('Parch')
 plt.ylabel('Not Survived Count', color='indigo')
-plt.tick_params(axis='y', labelcolor='indgo')
+plt.tick_params(axis='y', labelcolor='indigo')
 plt.legend(loc='upper right')
 
 # 두 번째 서브플롯 설정(막대 그래프)
