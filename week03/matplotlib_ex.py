0325f19bafb245946bd7edc3c5b89552549da176
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-29 18:28:12 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -344,4 +344,10 @@ fig, axes = plt.subplot(2, 1, figsize=(10, 10))
 
 # 첫 번째 서브플롯 설정(선 그래프)
 axes[0].plot(x, y1, '-o', color='indigo', markersize=7, linewidth=3, alpha=0.7, lable='Not Survived')
-axes[0]
+axes[0].set_xlabel('Parch')
+axes[0].set_ylabel('Not Survied Count', color='indigo')
+axes[0].tick_params(axis='y', labelcolor='indigo')
+axes[0].legend(loc='upper right')
+
+# 두 번째 서브플롯 설정 (막대 그래프)
+axes[1]
