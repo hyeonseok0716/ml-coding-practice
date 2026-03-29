b999a8801ea771a7c16b21afccded3df67d45a21
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-03-29 17:42:11 +0900

    Updated Changes

--- week03/matplotlib_ex.py
+++ week03/matplotlib_ex.py
@@ -175,3 +175,16 @@ plt.fill_between(survived_counts.index.astype(str), survived_counts[1],
                  color='purple', alpha=0.9, label='Survived')
 
 # 나이대별 사망자
+plt.fill_between(survived_counts.index.astype(str), survived_counts(0),
+                 color='hotpink', alpha=0.6, label='Not Survived')
+
+plt.title('Survival by Age Group on Titanic')
+plt.xlabel('Age')
+plt.ylabel('Count')
+plt.xticks(rotation=45)
+plt.legend()
+plt.grid(True)
+plt.savefig('Figure08.png')
+plt.close()
+
+## 
