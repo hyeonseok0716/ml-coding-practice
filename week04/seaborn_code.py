e4205e4c6c2fb324e72e2ef8ca69ffd86ad64217
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-05 19:45:42 +0900

    Updated Changes

--- week04/seaborn_code.py
+++ week04/seaborn_code.py
@@ -4,3 +4,13 @@ import seaborn as sns
 # **팁(tips) 데이터셋 불러오기**
 tips = sns.load.dataset('tips')
 print(tips.head())
+
+tips.info()
+
+# **범주형 변수 산점도 그래프**
+
+import matplotlib.pyplot as plt
+
+# figure에 2개의 서브 플롯을 생성
+fig = plt.figure(figsize=(15, 5))
+ax1 = fig.add_subplot(1, 2, 1)
