84e73920f23a4ebb4e75abc08f4ba4f5c225797c
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-05 21:52:30 +0900

    Updated Changes

--- week04/data_preprocessing.py
+++ week04/data_preprocessing.py
@@ -54,3 +54,9 @@ imputer.feature_names_in_
 housing_tr = pd.DataFrame(X, columns=housing_num.columns,
                           index=housing_num.index)
 housing_tr.loc[null_rows_idx].head()
+
+# 이상치 삭제
+from sklearn.ensemble import IsolationForest
+
+isolation_forest = IsolationForest(random_state=42)
+outlier_pred = isolation_forest.fit_predict(X)
