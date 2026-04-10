08d7e8b430575d495a67273633fdfa1aa0fd7d4e
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 10:29:22 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -20,7 +20,7 @@ def main():
     while ((jsonResponse != None) and (jsonResponse['display'] != 0)):
         for post in jsonResponse['items']:
             cnt += 1
-            getPostData(post, JsonResult, cnt)                # [CODE 3]
+            getPostData(post, jsonResult, cnt)                # [CODE 3]
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
