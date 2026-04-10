e51ca3fd5b8278ccc6a0ad67faa8f229fa5f62d7
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 10:18:51 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -42,4 +42,4 @@ def getNaverSearch(node, srcText, page_start, display):
     node = "/%s.json" % node
     parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
 
-    url1
+    ur
