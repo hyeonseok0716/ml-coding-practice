002dc1695f1f7eb019b97b538ff1a07f4455e377
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 11:17:28 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -5,3 +5,13 @@ import json
 import pandas as pd
 
 ServiceKey = "2dfe8c532dcafd667c07978e93429e8ca4aa343502dea9ab896109f69dd6ad07"
+
+"""### [CODE 0]"""
+
+def main():
+    jsonResult = []
+    result = []
+
+    print("<< 국내 입국한 외국인의 통계 데이터를 수집합니다. >>")
+    nat_cd = input('국가 코드를 입력하세요(중국: 112 / 일본: 130 / 미국: 275) :')
+    
