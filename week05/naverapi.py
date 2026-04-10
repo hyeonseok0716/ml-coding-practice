d121b3802022c60047a29d6c2d4c3a0556f703a9
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 10:02:21 +0900

    Updated Changes

--- week05/naverapi.py
+++ week05/naverapi.py
@@ -3,7 +3,9 @@ import urllib.request
 import datetime
 import json
 
-client_id = 
-client_secret =
+client_id = ''
+client_secret = ''
 
-def main
+def main():
+
+    node = 'news'                                             # 크롤링할 대상
