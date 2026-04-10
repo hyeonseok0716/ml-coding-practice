5ada0f4ded5eaf86f923a26eb3f27a9d79eaa296
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 11:27:28 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -34,4 +34,10 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
     for year in range(nStartYear, nEndYear+1):
         for month in range(1, 13):
             yyyymm = "{0}{1:0>2}".format(str(year), str(month))
-            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)
+            jsonData = getTourismStatsItem(yyyymm, nat_cd, ed_cd)     #[CODE 2]
+            if (jsonData['response']['header']['resultMsg'] == 'OK'):
+                #데이터가 없는 마지막 항목인 경우 ----------------------------
+                if jsonData['response']['body']['items'] == '':
+                    dataEND = "{0}{1:0>2}".format(str(year), str(month-1))
+                    print("데이터 없음.... \n제공되는 토예 데이터는 %s년 %s월까지입니다." % (str(year), str(month-1)))
+                    
