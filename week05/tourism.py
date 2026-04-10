a6dc06b5ca4ec3bc1dd5a15d7656bfb3c8c6430f
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-10 12:05:12 +0900

    Updated Changes

--- week05/tourism.py
+++ week05/tourism.py
@@ -48,5 +48,6 @@ def getTourismStatsService(nat_cd, ed_cd, nStartYear, nEndYear):
                 natName = natName.replace(' ', '')
                 num = jsonData['response']['body']['items']['item']['num']
                 ed = jsonData['response']['body']['items']['item']['ed']
-                print('[ %s_%s : %s ]' % (natName, 
+                print('[ %s_%s : %s ]' % (natName, yyyymm, num))
+                print()
                                           'nat_cd': nat_cd, 'yyyymm': yyyymm, 'visit_cnt': num))
