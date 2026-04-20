d172317f6c22c3580d0f96cb1cead666fac91f20
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 13:14:58 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -23,4 +23,17 @@ def main():
 
         start = jsonResponse['start'] + jsonResponse['display']
         jsonResponse = getNaverSearch(node, srcText, start, 100) # [CODE 2]
+
+    print('전체 검색 : %d 건' %total)
+
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding= 'utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent = 4, sort_keys = True,
+                              ensure_ascii = False)
         
+        outfile.write(jsonFile)
+
+    print("가져온 데이터 : %d 건" %(cnt))
+    print('%s_naver_%s.json SAVED' % (srcText, node))
+
+
+def getNaverSearch
