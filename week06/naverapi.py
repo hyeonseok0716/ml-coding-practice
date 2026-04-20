6e2b801d65aab0a1763443d1cf37d98e633ff561
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 13:24:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -55,4 +55,8 @@ def getRequestUrl(url):
     req = urllib.request.Request(url)
 
     req.add_header("X-Naver-Client-Id", client_id)
-    req.add_header("X-Naver-Client-Secret")
+    req.add_header("X-Naver-Client-Secret", client_secrset)
+
+    try:
+        response = urllib.request.urlopen(req)
+        if re
