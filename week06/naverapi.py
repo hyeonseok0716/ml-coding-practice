758af0605949fb2e2c2264870ab4c8a33df5ff6f
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 13:18:28 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -37,7 +37,9 @@ def main():
     print('%s_naver_%s.json SAVED' % (srcText, node))
 
 
-def getNaverSearch(ndoe, srcText, page_start, display):
+def getNaverSearch(node, srcText, page_start, display):
     base = "https://openapi.naver.com/v1/search"
     node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), page_start, display)
+
     
