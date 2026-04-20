cf16a91b4ecd58a96b9c94c4dc9f0ba5a642287f
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 16:45:01 +0900

    Updated Changes

--- week06/tourism.py
+++ week06/tourism.py
@@ -32,4 +32,6 @@ def main():
         result = []
 
         for year in range(nStartYear, nEndYear+1):
-            for ye
+            for month in range(1, 13):
+                yyyymm = "{0}{1:0>2}".format(str(year), str(month))
+                jsonData = getT
