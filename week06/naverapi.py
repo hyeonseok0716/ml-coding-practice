d9e1d0a0da4566b41b035b6f4d6febb64f4daf44
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 13:26:58 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -62,3 +62,10 @@ def getRequestUrl(url):
         if response.getcode() == 200:
             print("[%s] Url Request Success" % datetime.datetime.now())
             return response.read().decode('utf-8')
+    except Exception as e:
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+    
+
+def getPostData(post, JsonResult, cnt): 
