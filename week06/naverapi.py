042a6386d189f93569a4cd5f9f150d72068f3e08
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-20 13:09:58 +0900

    Updated Changes

--- week06/naverapi.py
+++ week06/naverapi.py
@@ -7,4 +7,10 @@ client_secrset = 'f9fYJiaBR9'
 
 def main():
 
-    node = 'news'
+    node = 'news'                                             # 크롤링할 대상
+    srcText = input('검색어를 입력하세요: ')
+
+    cnt = 0
+    jsonResult = []
+
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)
