81656636d49fd7f6c2f08ffb490a4caa0d608ebe
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-30 13:43:38 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -5,4 +5,8 @@ mnist = fetch_openml('mnist_784', as_frame=False)
 
 print(mnist.keys())  # data와 target만 사용
 
-x, y = mnist.data, mnist.target
+X, y = mnist.data, mnist.target
+print(X)
+print(X.shape)       # 28 x 28 개의 픽셀 특징을 가진 이미지 70,000개
+print(y)
+print(y.shape)
