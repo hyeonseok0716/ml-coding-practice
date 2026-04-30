bb0a890f049a7e0f2966b6bea956d3b3ceb163cc
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-04-30 13:40:38 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -3,4 +3,6 @@ from sklearn.datasets import fetch_openml
 
 mnist = fetch_openml('mnist_784', as_frame=False)
 
-print(mnist.keys()) 
+print(mnist.keys())  # data와 target만 사용
+
+x, y = mnist.data, mnist.target
