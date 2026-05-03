83760657f95b38ff2c5e2c1d121dfd0518f6f7e0
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-03 19:25:29 +0900

    Updated Changes

--- week07/classification.py
+++ week07/classification.py
@@ -33,10 +33,10 @@ plt.subplots_adjust(wspace=0, hspace=0)
 plt.show()
 
 # train/test 데이터셋 나누기
-X_train, X_test, y_train_, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 
 # 이진 분류기 훈련
-y_train_5 = (y_train_ == '5')  # 5는 True고, 다른 숫자는 모두 False
+y_train_5 = (y_train == '5')  # 5는 True고, 다른 숫자는 모두 False
 y_test_5 = (y_test == '5')
 
 from sklearn.linear_model import SGDClassifier
