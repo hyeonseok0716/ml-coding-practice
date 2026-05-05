2d9decad16312c40451006903a796a6ab6318c08
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-05 21:16:45 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -87,4 +87,4 @@ import seaborn as sns
 netflix = pd.read_csv('netflix_preprocessed.csv')
 
 # 넷플릭스 브랜드 상징 색깔 시각화
-sns.palplot(['#221f1f', '#b20710'm ])
+sns.palplot(['#221f1f', '#b20710', '#e50914', '#f5f5f1'])
