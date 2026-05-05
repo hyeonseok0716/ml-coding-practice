50c80f7ea348dad64ed04d5b143f92fdf5ac9eb1
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-05 21:27:15 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -131,3 +131,11 @@ genres = netflix['listed_in'].str.split(', ', expand=True).stack().value_counts(
 genres
 
 plt.figure(figsize=(12, 6))
+
+sns.barplot(x=genres.values, y=genres.index, hue=genres.index, palette='RdGy')
+
+plt.title('Distribution of Genres for Movies and TV Shows on Netflix', fontsize=16)
+plt.xlabel('Count', fontsize=14)
+plt.ylabel('Genre', fontsize=14)
+plt.grid(axis='x')
+plt.show()
