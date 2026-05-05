10010284334fe1437a8da86600ec6a13f70372d6
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-05 21:57:45 +0900

    Updated Changes

--- data_analysis/netflix_project.py
+++ data_analysis/netflix_project.py
@@ -207,3 +207,6 @@ text = str(list(netflix['description']))
 # 로고 이미지 열고 넘파이 배열로 변환
 mask = np.array(Image.open('netflix_logo.jpg'))
 
+# 워드 클라우드 색상맵 만들기
+cmap = plt.matplotlib.colors.LinearSegmentedColormap.from_list('', ['#221f1f','#b20710'])
+
