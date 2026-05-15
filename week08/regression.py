eb90d2db5acf71e6da9e749477d7eab8bc06cf42
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-15 20:45:13 +0900

    Updated Changes

---
+++ week08/regression.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt
+
+plt.rc('font', size=14)
+plt.rc('axes', labelsize=14, titlesize=14)
+plt.rc('legend', fontsize=14)
+plt.rc('xtick', labelsize=10)
