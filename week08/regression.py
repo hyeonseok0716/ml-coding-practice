a4b24ef2f612c8fd0de802e7d8c2f94f4181ff26
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-15 22:07:31 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -125,4 +125,19 @@ lasso_reg = Lasso(alpha=0.1)
 lasso_reg.fit(X, y)
 print(lasso_reg.predict([[1.5]]))
 
+# 엘라스틱넷
 
+from sklearn.linear_model import ElasticNet
+
+elastic_net = ElasticNet(alpha=0.1, l1_ratio=0.5)
+elastic_net.fit(X, y)
+print(elastic_net.predict([[1.5]]))
+
+# 로지스틱 회귀
+from sklearn.datasets import load_iris
+
+iris = load_iris(as_frame=True)
+list(iris)
+
+print(iris.data.head(3))
+print(iris.target.head(3))
