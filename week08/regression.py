d9aca0e3fe9b9aefde576145890471aa796a4894
서현석 <gustjr716xy2cb7sj4g@privaterelay.appleid.com>
2026-05-15 21:26:25 +0900

    Updated Changes

--- week08/regression.py
+++ week08/regression.py
@@ -65,4 +65,8 @@ X_poly = poly_features.fit_transform(X)
 print(X[0])
 print(X_poly[0])
 
+lin_reg = LinearRegression()
+lin_reg.fit(X_poly, y)
+lin_reg.intercept_, lin_reg.coef_
 
+X_new = np.linspace()
